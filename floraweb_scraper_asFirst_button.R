18ed53cf2a534800beccd7cca9296cc738a497f4
gimoya <kay.cichini@gmail.com>
2014-06-05 23:35:35 +0200

    some bug fixes

--- floraweb_scraper_asFirst_button.R
+++ floraweb_scraper_asFirst_button.R
@@ -8,6 +8,11 @@ require(tcltk)
 # Output: PDF to folder .~/FLORAWEB
 # Packages: XML, RCurl, jpeg
 # Licence: cc by-nc-sa
+# bug-fixes:
+# 05-06-2014 
+# ...replaced trim function with own function
+# ...replaced unneeded package 
+# ...fixed pdf-formatting 
 
 floraweb_scraper <- function(input) {
 
@@ -50,7 +55,7 @@ floraweb_scraper <- function(input) {
 		}
 	}
 
-	pkgs <- c("XML", "RCurl", "jpeg", "gregmisc")
+	pkgs <- c("XML", "RCurl", "jpeg")
 	instant_pkgs(pkgs)
 
 	# I didn't get around this encoding issue other than with gsub..
@@ -91,13 +96,12 @@ floraweb_scraper <- function(input) {
 		# check matching and assign to resulting dataframe:
 		match <- numeric()
 		for (i in 1:len) {
-			match[i] <- sum(unlist(strsplit(tolower(sp), " ")[i]) %in% unlist(strsplit(x, 
+			match[i] <- sum(unlist(strsplit(tolower(sp), " ")[i]) %in% unlist(strsplit(tolower(x), 
 				" ")) == 0)
 		}
-		df <- data.frame(sp, id_1, match)
 		
 		# select the one with best match:
-		sel <- id_1[rank(df$match)][1]
+		sel <- id_1[which(match == min(match))[1]]
 		
 		# build urls for retrieving species data
 		url <- paste("http://www.floraweb.de/pflanzenarten/druck.xsql?suchnr=", sel, 
@@ -117,8 +121,9 @@ floraweb_scraper <- function(input) {
 		infos_1 <- gsub(":\\s*", ": ", infos)
 		spl <- strsplit(infos_1, ":")
 		
+		trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 		infos_2 <- 
-		unlist(lapply(spl, function(x) {paste(x[1], ifelse(nchar(x[2]) > 1, trim(x[2]), "keine  Angaben"), sep = ": ")
+		unlist(lapply(spl, function(x) {paste("\n--", x[1], "--\n", ifelse(nchar(x[2]) > 1, trim(x[2]), "keine  Angaben"), sep = "")
 						}
 				)
 			)
@@ -131,7 +136,7 @@ floraweb_scraper <- function(input) {
 		download.file(img_url, "floraweb.jpg", mode = "wb")
 		
 		# open device:
-		pdf(paste(input1, "FloraWeb.pdf", sep = "+"), paper = "a4")
+		pdf(paste(input1, "FloraWeb.pdf", sep = "+"), paper = "a4r", width = 0, height = 0)
 		
 		# read image:
 		img <- readJPEG("floraweb.jpg")
@@ -152,9 +157,9 @@ floraweb_scraper <- function(input) {
 		c_adj <- c(0.5, 0.5)
 		
 		# plot text:
-		text(l, 0.9, paste("Eingabe = ", x, " / Gefunden = ", infos_2[1], sep = ""), font = 2, 
+		text(l, 0.9, paste("Eingabe = ", x, "\nGefunden = ", infos_2[1], sep = ""), font = 2, 
 			adj = c_adj, cex = 0.7)
-		text(l, 0.5, paste(strwrap(infos_2[-1], width = 112), collapse = "\n"), adj = c_adj, 
+		text(l, 0.5, paste(unlist(strsplit(infos_2[-1], ": ")), collapse = "\n"), adj = c_adj, 
 			cex = 0.7)
 		
 		# Credit:
@@ -177,7 +182,7 @@ PressedOK <- function()
 {   
     floraweb_scraper()
     invisible(lapply(dir(pattern = ".pdf"), FUN = shell.exec))
-    tkmessageBox(message="PDFs wurden erzeugt!")
+    tkmessageBox(message="PDFs wurden erzeugt!\nund unter .~/Documents/FLORAWEB/.. abgelegt")
     q(save="no")
     tkdestroy(tt)
 }
